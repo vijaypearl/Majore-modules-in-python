d7892f65d65d75b1dd0dc5f985553742c1894326
vijay kalimuthu <47220243+vijaypearl@users.noreply.github.com>
2021-10-17 09:11:02 +0530

    Add files via upload
    Here easily able to recognize  the desired pattern using "re" module of python

---
+++ re_module.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 08:45:14 2021
+
+@author: vijay kalimuthu
+"""
+import re
+
+#segment1 - using search method
+
+paragraph = "please all of you if you have any issue contact me in vijaykala15me168@gmail.com "
+req_format = re.compile("[a-zA-Z0-9]+@+[a-zA-Z]+\.[a-zA-Z]+") #email format
+output= req_format.search(paragraph)
+print(output)  # output = vijaykala15me168@gmail.com
+
+
+#segment2 - using findall method
+
+para = "I love you so much d udaya"
+condition = re.compile("[a-zA-Z0-9]+")
+ans = condition.findall(para)
+print(ans[0])  # ans = ['I', 'love', 'you', 'so', 'much', 'd', 'udaya'] 
